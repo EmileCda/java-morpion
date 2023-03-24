965ea31c9b869c97812b35d66d1749e6fee4a207
emileCda <emile.cda@gmail.com>
2023-03-24 09:13:08 +0100

    develop ready

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+
+public class Main {
+// ----------------------------------------------------------------------------
+    public static int template(String reponse) {
+        return 0;
+    }
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    public static void main(String[] args) {
+            Scanner myScan = new Scanner(System.in);
+
+            System.out.println("Morpion");
+    }
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+}
+
+
